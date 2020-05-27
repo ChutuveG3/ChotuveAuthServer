e9e0cdeeaffe848cd648d8f8448d8f53573b2b89
Santiago Almejun <santi.alme@gmail.com>
2020-05-27 15:57:40 -0300

    added 1h expiration time to token

--- app/services/jwt.js
+++ app/services/jwt.js
@@ -3,7 +3,8 @@ const moment = require('moment');
 const { invalidTokenError } = require('../errors');
 const { secret } = require('../../config').common.session;
 
-exports.generateTokenFromEmail = ({ email }) => jwt.sign({ sub: email, iat: moment().unix() }, secret);
+exports.generateTokenFromEmail = ({ email }) =>
+  jwt.sign({ sub: email, iat: moment().unix() }, secret, { expiresIn: '1h' });
 
 exports.decodeToken = token =>
   jwt.verify(token, secret, (verifyError, decodedToken) => {
